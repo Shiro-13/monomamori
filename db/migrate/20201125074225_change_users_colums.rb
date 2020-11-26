435e5c18898a11127054b3b8e88395c17e1116a6
EC2 Default User <shirai0119@gmail.com>
2020-11-26 05:34:03 +0000

    [add]ログイン機能、トップページ

---
+++ db/migrate/20201125074225_change_users_colums.rb
@@ -0,0 +1,7 @@
+class ChangeUsersColums < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :last_name, :string
+    remove_column :users, :first_name, :string
+    add_column :users, :name, :string
+  end
+end
